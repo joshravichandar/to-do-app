14fbf8a2673e5aaf2ecc09db35468029dbd6fb45
joshravichandar <joshravichandar@gmail.com>
2023-11-15 17:44:48 -0500

    add functionality adds to entry, not list

--- ToDoApp.py
+++ ToDoApp.py
@@ -2,6 +2,26 @@
 from tkinter import *
 from tkinter import ttk as tk
 
+GLOBAL_ITEMS = []
+
+# https://tkdocs.com/tutorial/morewidgets.html
+# link to tkinter docs for setting listbox
+def add_press(added_item, gl_items):
+    gl_items = GLOBAL_ITEMS
+    gl_items.append(added_item)
+    added_item.set(gl_items)
+    
+    pass
+
+def delete_press():
+    pass
+
+def up_press():
+    pass
+
+def down_press():
+    pass
+
 
 
 
@@ -15,16 +35,21 @@ if __name__ == '__main__':
     
 
     # create list and add to grid
-    to_do_list = Listbox(main_frame, width=20)
+    
+    items = GLOBAL_ITEMS
+    ITEMS_STRING = StringVar(value=items)
+    to_do_list = Listbox(main_frame, width=20, listvariable=ITEMS_STRING)
     to_do_list.grid(column=0, row=0,rowspan=5 ,pady=5)
+    ITEMS_STRING.set(items)
+    
 
     # create var for entering new item, text field, add to grid
-    new_item = StringVar()
+    new_item = StringVar(value=GLOBAL_ITEMS)
     enter_item = tk.Entry(main_frame, width=20, textvariable=new_item, justify=CENTER)
     enter_item.grid(column=0, row=6)
 
     # create all buttons and add to grid
-    add = tk.Button(main_frame,width=20 ,text="Add Item").grid(column=0, row=7)
+    add = tk.Button(main_frame,width=20 ,text="Add Item",command=lambda: add_press(new_item, items)).grid(column=0, row=7)
     delete = tk.Button(main_frame, text="Delete").grid(column=1, row=3)
     up = tk.Button(main_frame, text="Up").grid(column=1, row=0)
     down = tk.Button(main_frame, text="Down").grid(column=1, row=1)
