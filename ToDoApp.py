a6864c31a0b34f3590072c422fee112e8fd7f10c
joshravichandar <joshravichandar@gmail.com>
2023-11-17 11:26:17 -0500

    fixed add_press functionality

--- ToDoApp.py
+++ ToDoApp.py
@@ -2,16 +2,19 @@
 from tkinter import *
 from tkinter import ttk as tk
 
-GLOBAL_ITEMS = []
+TO_DO_ITEMS = []
 
 # https://tkdocs.com/tutorial/morewidgets.html
 # link to tkinter docs for setting listbox
-def add_press(added_item, gl_items):
-    gl_items = GLOBAL_ITEMS
-    gl_items.append(added_item)
-    added_item.set(gl_items)
-    
-    pass
+def add_press():
+    # take in item string from entry field
+    item = new_item.get()
+    # append to list first, append from list to listbox, reset to empty
+    if (item != ""):
+        TO_DO_ITEMS.append(item)
+        to_do_list.insert(len(TO_DO_ITEMS), item)
+        new_item.set("")
+        
 
 def delete_press():
     pass
@@ -35,21 +38,18 @@ if __name__ == '__main__':
     
 
     # create list and add to grid
-    
-    items = GLOBAL_ITEMS
-    ITEMS_STRING = StringVar(value=items)
-    to_do_list = Listbox(main_frame, width=20, listvariable=ITEMS_STRING)
+    to_do_list = Listbox(main_frame, width=20)
     to_do_list.grid(column=0, row=0,rowspan=5 ,pady=5)
-    ITEMS_STRING.set(items)
+    
     
 
     # create var for entering new item, text field, add to grid
-    new_item = StringVar(value=GLOBAL_ITEMS)
+    new_item = StringVar(value=TO_DO_ITEMS)
     enter_item = tk.Entry(main_frame, width=20, textvariable=new_item, justify=CENTER)
     enter_item.grid(column=0, row=6)
 
     # create all buttons and add to grid
-    add = tk.Button(main_frame,width=20 ,text="Add Item",command=lambda: add_press(new_item, items)).grid(column=0, row=7)
+    add = tk.Button(main_frame,width=20 ,text="Add Item",command=add_press).grid(column=0, row=7)
     delete = tk.Button(main_frame, text="Delete").grid(column=1, row=3)
     up = tk.Button(main_frame, text="Up").grid(column=1, row=0)
     down = tk.Button(main_frame, text="Down").grid(column=1, row=1)
